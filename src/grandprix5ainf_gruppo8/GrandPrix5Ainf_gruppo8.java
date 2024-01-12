58f4924c1887feaa10ef79fdbd4785c80c3783df
paola bachiorri <sillagabriele.05@gmail.com>
2024-01-12 14:07:52 +0100

    primo commit

---
+++ src/grandprix5ainf_gruppo8/GrandPrix5Ainf_gruppo8.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grandprix5ainf_gruppo8;
+
+/**
+ *
+ * @author gabri
+ */
+public class GrandPrix5Ainf_gruppo8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
