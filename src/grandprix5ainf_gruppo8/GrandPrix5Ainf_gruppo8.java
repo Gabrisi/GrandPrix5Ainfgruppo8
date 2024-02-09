8e7701d027aa2e6434fe3e203c8d703fc9f04e88
Manuel Mercadante <manuelscuola2025@gmail.com>
2024-02-09 14:23:35 +0100

    prova

---
+++ src/grandprix5ainf_gruppo8/GrandPrix5Ainf_gruppo8.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grandprix5ainf_gruppo8;
+
+/**
+ *
+ * @author gabri
+ */
+public class GrandPrix5Ainf_gruppo8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String marca = null; //assegnare il valore dal file a questa variabile
+        //istanziamo gli oggetti
+        car macchina = new car(marca);
+    }
+    
+}
---
+++ src/grandprix5ainf_gruppo8/car.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grandprix5ainf_gruppo8;
+
+/**
+ *
+ * @author gabri
+ */
+public class car {
+    String marca;
+    public void car(String marca){
+        this.marca=marca;
+    }
+}
