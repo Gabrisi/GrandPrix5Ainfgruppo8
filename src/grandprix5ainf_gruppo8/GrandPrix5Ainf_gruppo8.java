e675779d3e561b8968e51f6af9e83c509625f8ac
Manuel Mercadante <manuelscuola2025@gmail.com>
2024-02-09 14:29:53 +0100

    prova2

--- src/grandprix5ainf_gruppo8/GrandPrix5Ainf_gruppo8.java
+++ src/grandprix5ainf_gruppo8/GrandPrix5Ainf_gruppo8.java
@@ -11,7 +11,7 @@ package grandprix5ainf_gruppo8;
 public class GrandPrix5Ainf_gruppo8 {
 
     /**
-     * @param args the command line arguments
+     * @param args the command line arguments..
      */
     public static void main(String[] args) {
         String marca = null; //assegnare il valore dal file a questa variabile
