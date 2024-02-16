552e58523f5c17e264a6b96cfcf6754abd857717
RamacciCri <cristianramacci56@gmail.com>
2024-02-16 14:45:51 +0100

    Aggiunta del metodo per cifrare le password.

--- src/grandprix5ainf_gruppo8/GrandPrix5Ainf_gruppo8.java
+++ src/grandprix5ainf_gruppo8/GrandPrix5Ainf_gruppo8.java
@@ -6,6 +6,7 @@ package grandprix5ainf_gruppo8;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
@@ -18,12 +19,41 @@ public class GrandPrix5Ainf_gruppo8 {
         String nome = null;
         String cognome = null;
         String giocatore = null;
+        String passwordGiocatore = null;
         //istanziamo gli oggetti
         car macchina1 = new car(marca);
         driver guidatore1 = new driver(nome,cognome);
         player giocatore1 = new player(giocatore,"Dati.json");
         giocatore1.leggi();
         System.out.println(giocatore1.nomeFile);
+        
+        ArrayList<Vigenere> quadranti = new ArrayList<Vigenere>(); 
+
+        Matrice matrix=new Matrice("TPSIT");
+    
+        Vigenere quadrante_1=new Vigenere(0,12,0,12,matrix);
+        quadranti.add(quadrante_1);
+    
+        Vigenere quadrante_2=new Vigenere(0,12,12,26,matrix);
+        quadranti.add(quadrante_2);
+    
+        Vigenere quadrante_3=new Vigenere(12,26,0,12,matrix);
+        quadranti.add(quadrante_3);
+    
+        Vigenere quadrante_4=new Vigenere(12,26,12,26,matrix);
+        quadranti.add(quadrante_4);
+        
+        for(Vigenere v:quadranti){
+         Thread t= new Thread(v);
+         t.start();
+         try {
+           t.join();
+         }catch (InterruptedException ex) {
+             System.err.println("Errore metodo join");
+         }
+        }  
+        
+        String passwordCifrata = matrix.cifra(passwordGiocatore);
         
     }
     
