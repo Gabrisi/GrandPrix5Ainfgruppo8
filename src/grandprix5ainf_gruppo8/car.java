99a334cb5d087911f08c2ddfcb1bebb39b1c460e
Silvio <152089840+SilvioOddo@users.noreply.github.com>
2024-03-03 18:37:51 +0100

    Update car.java

--- src/grandprix5ainf_gruppo8/car.java
+++ src/grandprix5ainf_gruppo8/car.java
@@ -4,13 +4,48 @@
  */
 package grandprix5ainf_gruppo8;
 
+import java.util.Random;
+
 /**
  *
  * @author gabri
  */
-public class car {
-    String marca;
-    public car(String marca){
-        this.marca=marca;
+
+public class car extends Thread {
+    public String marca;
+    public String modello;
+    public int anno;
+    public int velocita;
+    
+
+    public car(String marca, String modello, int anno, int velocita) {
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.velocita = velocita;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
     }
 }
